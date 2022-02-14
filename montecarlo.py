b4f5bf4a95d655ba2f478054af86443ec2f1b146
Ronvic2303 <58205103+ronvic12@users.noreply.github.com>
2022-02-14 08:39:22 -0800

    first commit

---
+++ montecarlo.py
@@ -0,0 +1,58 @@
+import numpy as np
+import random
+import sys
+import signal
+from players import connect4Player
+from copy import deepcopy
+
+class monteCarloAI(connect4Player):
+
+	def play(self, env, move):
+		random.seed(self.seed)
+		# Find legal moves
+		env = deepcopy(env)
+		env.visualize = False
+		possible = env.topPosition >= 0
+		indices = []
+		for i, p in enumerate(possible):
+			if p: indices.append(i)
+		# Init fitness trackers
+		vs = np.zeros(7)
+		# Play until told to stop
+		counter = 0
+		while True:
+			first_move = random.choice(indices)
+			turnout = self.playRandomGame(deepcopy(env), first_move)
+			if turnout == self.position: #could be the max
+				vs[first_move] += 1
+			elif turnout != 0:
+				vs[first_move] -= 1
+			if counter % 100 == 0:
+				move[:] = [np.argmax(vs)]
+			counter += 1
+	#use this idea 
+	def playRandomGame(self, env, first_move):
+		switch = {1:2,2:1}
+		move = first_move
+		player = self.position
+		self.simulateMove(env, move, player)
+		while not env.gameOver(move, player):
+			player = switch[player]
+			possible = env.topPosition >= 0
+			indices = []
+			for i, p in enumerate(possible):
+				if p: indices.append(i)
+			move = random.choice(indices)
+			self.simulateMove(env, move, player)
+		if len(env.history[0]) == 42: return 0
+		return player
+
+	def simulateMove(self, env, move, player):
+		env.board[env.topPosition[move]][move] = player
+		env.topPosition[move] -= 1
+		env.history[0].append(move)
+
+	def signal_handler(self):
+		print("SIGTERM ENCOUNTERED")
+		sys.exit(0)
+
---
+++ players.py
@@ -0,0 +1,283 @@
+from copy import deepcopy
+import random
+import sys
+import time
+import pygame
+import math
+
+class connect4Player(object):
+    def __init__(self, position, seed=0):
+        self.position = position
+        self.opponent = None
+        self.seed = seed
+        random.seed(seed)
+
+    def play(self, env, move):
+        move = [-1]
+
+class human(connect4Player):
+
+    def play(self, env, move):
+        move[:] = [int(input('Select next move: '))]
+        while True:
+            if int(move[0]) >= 0 and int(move[0]) <= 6 and env.topPosition[int(move[0])] >= 0:
+                break
+            move[:] = [int(input('Index invalid. Select next move: '))]
+
+class human2(connect4Player):
+
+    def play(self, env, move):
+        done = False
+        while(not done):
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    sys.exit()
+
+                if event.type == pygame.MOUSEMOTION:
+                    pygame.draw.rect(screen, BLACK, (0,0, width, SQUARESIZE))
+                    posx = event.pos[0]
+                    if self.position == 1:
+                        pygame.draw.circle(screen, RED, (posx, int(SQUARESIZE/2)), RADIUS)
+                    else: 
+                        pygame.draw.circle(screen, YELLOW, (posx, int(SQUARESIZE/2)), RADIUS)
+                pygame.display.update()
+
+                if event.type == pygame.MOUSEBUTTONDOWN:
+                    posx = event.pos[0]
+                    col = int(math.floor(posx/SQUARESIZE))
+                    move[:] = [col]
+                    done = True
+
+class randomAI(connect4Player):
+
+    def play(self, env, move):
+        possible = env.topPosition >= 0
+        indices = []
+        for i, p in enumerate(possible):
+            if p: indices.append(i)
+        move[:] = [random.choice(indices)]
+
+class stupidAI(connect4Player):
+
+    def play(self, env, move):
+        possible = env.topPosition >= 0
+        indices = []
+        for i, p in enumerate(possible):
+            if p: indices.append(i)
+        if 3 in indices:
+            move[:] = [3]
+        elif 2 in indices:
+            move[:] = [2]
+        elif 1 in indices:
+            move[:] = [1]
+        elif 5 in indices:
+            move[:] = [5]
+        elif 6 in indices:
+            move[:] = [6]
+        else:
+            move[:] = [0]
+
+class minimaxAI(connect4Player):
+    
+    
+
+    def play(self, env, move):
+         # env.turnPlayer.position == 1 set is MaxPlayer to true False otherwise
+        if env.turnPlayer.position == 1:
+            MaxPlayer = True
+        else:
+            MaxPlayer = False
+            
+            
+            
+        current_state = deepcopy(env)
+        
+        
+        move[:]=[self.minimax(current_state,1,MaxPlayer)]
+        
+        
+        
+    def winning_move(self,board, piece):
+        # Check horizontal locations for win
+        for c in range(COLUMN_COUNT-3):
+            for r in range(ROW_COUNT):
+                if board[r][c] == piece and board[r][c+1] == piece and board[r][c+2] == piece and board[r][c+3] == piece:
+                    return True
+
+    # Check vertical locations for win
+        for c in range(COLUMN_COUNT):
+            for r in range(ROW_COUNT-3):
+                if board[r][c] == piece and board[r+1][c] == piece and board[r+2][c] == piece and board[r+3][c] == piece:
+                    return True
+
+    # Check positively sloped diaganols
+        for c in range(COLUMN_COUNT-3):
+            for r in range(ROW_COUNT-3):
+                if board[r][c] == piece and board[r+1][c+1] == piece and board[r+2][c+2] == piece and board[r+3][c+3] == piece:
+                    return True
+
+    # Check negatively sloped diaganols
+        for c in range(COLUMN_COUNT-3):
+            for r in range(3, ROW_COUNT):
+                if board[r][c] == piece and board[r-1][c+1] == piece and board[r-2][c+2] == piece and board[r-3][c+3] == piece:
+                    return True
+        
+        
+    def minimax(self,env,depth,maximizingPlayer):
+        playerID = env.turnPlayer.position
+        playerhistory = env.history[0]
+        
+        if len(playerhistory) != 0:
+         if depth == 0 or env.gameOver(env.history[0][-1],playerID): 
+                
+                evaluation = self.eval(env,env.turnPlayer)-self.eval(env,env.turnPlayer.opponent)
+                return evaluation
+        
+        if maximizingPlayer == True:
+          
+            value = -math.inf
+            best_move= random.choice(self.valid_locations(env))
+            
+            for col in self.valid_locations(env): # valid_locations returns the indices of the possible columns
+                
+                board_copy = deepcopy(env) # double check this one
+                
+                self.simulateMove(board_copy,col, env.turnPlayer.position)
+                
+                child_val = self.minimax(board_copy,depth-1,False)
+                
+                if child_val > value:
+                    value = child_val
+                    best_move = col # best_move = child_move
+                   
+            return best_move
+        else: # minimizing player
+            value = math.inf
+            best_move= random.choice(self.valid_locations(env))
+            
+            for col in self.valid_locations(env):
+                
+                board_copy = deepcopy(env) # double check this one
+                
+                self.simulateMove(board_copy,col, env.turnPlayer.opponent.position)
+                child_val = self.minimax(board_copy,depth-1,True)
+                
+               
+                if child_val < value:
+                    value = child_val
+                    best_move = col # best_move = child_move
+            
+            return best_move
+        
+    
+    
+    
+    def simulateMove(self, env, move, player):
+       
+        env.board[env.topPosition[move]][move] = player
+        
+        env.topPosition[move] -= 1
+        env.history[0].append(move) 
+  
+  
+    def eval(self,env,piece):
+        #print(piece.position) # count all of the pieces of 1 or 2 tokens. 
+        score = 0
+    #middle column  
+        middle_array = [int(i) for i in list(env.board[:,COLUMN_COUNT//2])]
+        
+        middle_count = middle_array.count(piece.position)
+        score += middle_count * 3
+        
+    #Score horizontal
+        for r in range(ROW_COUNT):
+            row_array = [int(i) for i in list(env.board[r,:])]
+            #print(row_array)
+            for c in range(COLUMN_COUNT-3):
+            # list of row arrays
+                # print("Token list")
+           
+                token_list= row_array[c:c+4]
+                # print(token_list)
+                score+=self.eval_score_method(token_list,piece.position)
+                
+      
+    #Score vertical 
+        for c in range(COLUMN_COUNT):
+            col_array = [int(i) for i in list(env.board[:,c])]
+            # print("columns")
+            # print(col_array)
+            for r in range(ROW_COUNT-3): # max of four columns
+                token_list = col_array[r:r+4]
+                score+=self.eval_score_method(token_list,piece.position)
+       
+           
+    # score positive slope diagonal 
+        for r in range(ROW_COUNT-3):
+             for c in range(COLUMN_COUNT-3):
+                token_list=[env.board[r+i][c+i] for i in range(4)]
+                score+=self.eval_score_method(token_list,piece.position)
+            
+    # negative slope score diagonal 
+        for r in range(ROW_COUNT-3):
+            for c in range(COLUMN_COUNT-3):
+                token_list=[env.board[r+3-i][c+i] for i in range(4)]
+                score+=self.eval_score_method(token_list,piece.position)
+                
+        
+        return score
+        
+        
+#helper functions and eval functions
+# need for yourself and for the opponent 
+    def valid_locations(self,env):
+        possible = env.topPosition >= 0
+        indices = []
+        for i, p in enumerate(possible):
+            if p: indices.append(i)
+        return indices
+
+    def eval_score_method(self,token_list,piece):
+        score = 0
+        if token_list.count(piece) == 4:
+            score += 100 
+        elif token_list.count(piece) == 3 and token_list.count(0) == 1: # if there are three in a row tokens
+            score += 5 
+        elif token_list.count(piece) == 2 and token_list.count(0) == 2: # if there are two in a row tokens
+            score += 2
+        return score
+
+
+
+
+class alphaBetaAI(connect4Player):
+
+    def play(self, env, move):
+        pass
+
+
+SQUARESIZE = 100
+BLUE = (0,0,255)
+BLACK = (0,0,0)
+RED = (255,0,0)
+YELLOW = (255,255,0)
+
+ROW_COUNT = 6
+COLUMN_COUNT = 7
+
+pygame.init()
+
+SQUARESIZE = 100
+
+width = COLUMN_COUNT * SQUARESIZE
+height = (ROW_COUNT+1) * SQUARESIZE
+
+size = (width, height)
+
+RADIUS = int(SQUARESIZE/2 - 5)
+
+screen = pygame.display.set_mode(size)
+
+
+
+
