b8483dee815a2f314bfafdb5b30e4a0e95110048
Ronvic2303 <58205103+ronvic12@users.noreply.github.com>
2022-02-15 21:30:12 -0800

    Update players.py

--- players.py
+++ players.py
@@ -82,46 +82,14 @@ class minimaxAI(connect4Player):
     
 
     def play(self, env, move):
-         # env.turnPlayer.position == 1 set is MaxPlayer to true False otherwise
         if env.turnPlayer.position == 1:
             MaxPlayer = True
         else:
             MaxPlayer = False
             
-            
-            
         current_state = deepcopy(env)
-        
-        
         move[:]=[self.minimax(current_state,1,MaxPlayer)]
-        
-        
-        
-    def winning_move(self,board, piece):
-        # Check horizontal locations for win
-        for c in range(COLUMN_COUNT-3):
-            for r in range(ROW_COUNT):
-                if board[r][c] == piece and board[r][c+1] == piece and board[r][c+2] == piece and board[r][c+3] == piece:
-                    return True
-
-    # Check vertical locations for win
-        for c in range(COLUMN_COUNT):
-            for r in range(ROW_COUNT-3):
-                if board[r][c] == piece and board[r+1][c] == piece and board[r+2][c] == piece and board[r+3][c] == piece:
-                    return True
-
-    # Check positively sloped diaganols
-        for c in range(COLUMN_COUNT-3):
-            for r in range(ROW_COUNT-3):
-                if board[r][c] == piece and board[r+1][c+1] == piece and board[r+2][c+2] == piece and board[r+3][c+3] == piece:
-                    return True
-
-    # Check negatively sloped diaganols
-        for c in range(COLUMN_COUNT-3):
-            for r in range(3, ROW_COUNT):
-                if board[r][c] == piece and board[r-1][c+1] == piece and board[r-2][c+2] == piece and board[r-3][c+3] == piece:
-                    return True
-        
+        print("finished")
         
     def minimax(self,env,depth,maximizingPlayer):
         playerID = env.turnPlayer.position
@@ -181,7 +149,6 @@ class minimaxAI(connect4Player):
   
   
     def eval(self,env,piece):
-        #print(piece.position) # count all of the pieces of 1 or 2 tokens. 
         score = 0
     #middle column  
         middle_array = [int(i) for i in list(env.board[:,COLUMN_COUNT//2])]
@@ -192,21 +159,14 @@ class minimaxAI(connect4Player):
     #Score horizontal
         for r in range(ROW_COUNT):
             row_array = [int(i) for i in list(env.board[r,:])]
-            #print(row_array)
             for c in range(COLUMN_COUNT-3):
-            # list of row arrays
-                # print("Token list")
-           
                 token_list= row_array[c:c+4]
-                # print(token_list)
                 score+=self.eval_score_method(token_list,piece.position)
                 
       
     #Score vertical 
         for c in range(COLUMN_COUNT):
             col_array = [int(i) for i in list(env.board[:,c])]
-            # print("columns")
-            # print(col_array)
             for r in range(ROW_COUNT-3): # max of four columns
                 token_list = col_array[r:r+4]
                 score+=self.eval_score_method(token_list,piece.position)
@@ -229,8 +189,9 @@ class minimaxAI(connect4Player):
         
         
 #helper functions and eval functions
-# need for yourself and for the opponent 
+# need for yourself and for the opponent
     def valid_locations(self,env):
+        # arrange them which is the best move first. for alpha beta pruning. 
         possible = env.topPosition >= 0
         indices = []
         for i, p in enumerate(possible):
@@ -253,57 +214,24 @@ class minimaxAI(connect4Player):
 class alphaBetaAI(connect4Player):
 
     def play(self, env, move):
-             # env.turnPlayer.position == 1 set is MaxPlayer to true False otherwise
         if env.turnPlayer.position == 1:
             MaxPlayer = True
         else:
             MaxPlayer = False
             
-            
-            
         current_state = deepcopy(env)
-        
-        
         move[:]=[self.alphabeta(current_state,1,-math.inf,math.inf,MaxPlayer)]
-        
-        
-        
-    def winning_move(self,board, piece):
-        # Check horizontal locations for win
-        for c in range(COLUMN_COUNT-3):
-            for r in range(ROW_COUNT):
-                if board[r][c] == piece and board[r][c+1] == piece and board[r][c+2] == piece and board[r][c+3] == piece:
-                    return True
 
-    # Check vertical locations for win
-        for c in range(COLUMN_COUNT):
-            for r in range(ROW_COUNT-3):
-                if board[r][c] == piece and board[r+1][c] == piece and board[r+2][c] == piece and board[r+3][c] == piece:
-                    return True
 
-    # Check positively sloped diaganols
-        for c in range(COLUMN_COUNT-3):
-            for r in range(ROW_COUNT-3):
-                if board[r][c] == piece and board[r+1][c+1] == piece and board[r+2][c+2] == piece and board[r+3][c+3] == piece:
-                    return True
 
-    # Check negatively sloped diaganols
-        for c in range(COLUMN_COUNT-3):
-            for r in range(3, ROW_COUNT):
-                if board[r][c] == piece and board[r-1][c+1] == piece and board[r-2][c+2] == piece and board[r-3][c+3] == piece:
-                    return True
-        
-        
     def alphabeta(self,env,depth,alpha,beta,maximizingPlayer):
         playerID = env.turnPlayer.position
         playerhistory = env.history[0]
         
         if len(playerhistory) != 0:
          if depth == 0 or env.gameOver(env.history[0][-1],playerID): 
-                
-                evaluation = self.eval(env,env.turnPlayer)-self.eval(env,env.turnPlayer.opponent)
-                return evaluation
-        
+                    evaluation = self.eval(env,env.turnPlayer)-self.eval(env,env.turnPlayer.opponent)
+                    return evaluation        
         if maximizingPlayer == True:
           
             value = -math.inf
@@ -315,14 +243,15 @@ class alphaBetaAI(connect4Player):
                 
                 self.simulateMove(board_copy,col, env.turnPlayer.position)
                 
-                child_val = self.alphabeta(board_copy,alpha,beta,depth-1,False)
+                child_val = self.alphabeta(board_copy,depth-1,alpha,beta,False)
                 
                 if child_val > value:
                     value = child_val
-                    best_move = col # best_move = child_move
-                alpha = max(alpha,value)
-                if alpha>= beta:
-                    break;
+                    best_move = col 
+
+                if value >= beta:
+                    break
+                alpha=max(alpha,value)
             return best_move
         else: # minimizing player
             value = math.inf
@@ -333,15 +262,14 @@ class alphaBetaAI(connect4Player):
                 board_copy = deepcopy(env) # double check this one
                 
                 self.simulateMove(board_copy,col, env.turnPlayer.opponent.position)
-                child_val = self.alphabeta(board_copy,alpha,beta,depth-1,True)
-                
+                child_val = self.alphabeta(board_copy,depth-1,alpha,beta,True)
                 if child_val < value:
                     value = child_val
                     best_move = col # best_move = child_move
-                beta = min(beta,value)
-                if alpha>= beta:
-                    break;
-            
+                if value <= alpha:
+                    break
+                beta=min(beta,value)
+
             return best_move
         
     
@@ -356,7 +284,6 @@ class alphaBetaAI(connect4Player):
   
   
     def eval(self,env,piece):
-        #print(piece.position) # count all of the pieces of 1 or 2 tokens. 
         score = 0
     #middle column  
         middle_array = [int(i) for i in list(env.board[:,COLUMN_COUNT//2])]
@@ -367,21 +294,14 @@ class alphaBetaAI(connect4Player):
     #Score horizontal
         for r in range(ROW_COUNT):
             row_array = [int(i) for i in list(env.board[r,:])]
-            #print(row_array)
             for c in range(COLUMN_COUNT-3):
-            # list of row arrays
-                # print("Token list")
-           
                 token_list= row_array[c:c+4]
-                # print(token_list)
                 score+=self.eval_score_method(token_list,piece.position)
                 
       
     #Score vertical 
         for c in range(COLUMN_COUNT):
             col_array = [int(i) for i in list(env.board[:,c])]
-            # print("columns")
-            # print(col_array)
             for r in range(ROW_COUNT-3): # max of four columns
                 token_list = col_array[r:r+4]
                 score+=self.eval_score_method(token_list,piece.position)
@@ -404,26 +324,22 @@ class alphaBetaAI(connect4Player):
         
         
 #helper functions and eval functions
-# need for yourself and for the opponent 
+# need for yourself and for the opponent
+#sucessor function
     def valid_locations(self,env):
+        # arrange them which is the best move first. for alpha beta pruning. 
         possible = env.topPosition >= 0
         indices = []
-        order = [3,2,4,1,5,0,6]
-        print("possible locations",possible)
+        order =[3,2,4,1,5,0,6]
+        possible =[possible[i] for i in order]
         for i, p in enumerate(possible):
-            if p: indices.append(i) # if this is true, you only append that to the list.
-        #print("indices are",indices)
-        print("p is ",p)
-        indices=[indices[i] for i in order if p in indices.append(i)]
-        #[3,2,4,1,5,0,6]
-        
-        print("Indices are",indices)
+            if p: indices.append(i)
         return indices
 
     def eval_score_method(self,token_list,piece):
         score = 0
         if token_list.count(piece) == 4:
-            score += 100 
+            score += 100
         elif token_list.count(piece) == 3 and token_list.count(0) == 1: # if there are three in a row tokens
             score += 5 
         elif token_list.count(piece) == 2 and token_list.count(0) == 2: # if there are two in a row tokens
